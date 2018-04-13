1468d95d73a4cf3e758a1a5decc7d116b14d852b
asanghehe <413820400@qq.com>
2018-04-13 16:17:00 +0800

    Update card-write.py

--- card-write.py
+++ card-write.py
@@ -13,7 +13,8 @@ if not error:
       # Auth for block 10 (block 2 of sector 2) using default shipping key A
       if not rdr.card_auth(rdr.auth_a, 10, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], uid):
         # This will print something like (False, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-        print("Writing block 10: " + str(rdr.write(10, [233, 33, 33, 33, 33, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 255])))
+        for sblock in range(0,15):
+          print("Writing block 10: " + str(rdr.write(10, [233, 33, 33, 33, 33, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 255])))
         # Always stop crypto1 when done working
         rdr.stop_crypto()
 

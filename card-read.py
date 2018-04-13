c9b94096af6d29a35004491b6a0e041f5716931f
asanghehe <413820400@qq.com>
2018-04-13 16:42:29 +0800

    Update card-read.py

--- card-read.py
+++ card-read.py
@@ -15,9 +15,10 @@ if not error:
       # Auth for block 10 (block 2 of sector 2) using default shipping key A
       if not rdr.card_auth(rdr.auth_a, 10, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], uid):
         # This will print something like (False, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-        for sblock in range(0,15):
-          print("reading "+str(sblock)+" block data: \t "+str(rdr.read(sblock)))
-          time.sleep(0.1)
+        for sector in range(0,15):
+          for block in range(0,2):
+            print("reading "+str(sector * 16 + block)+" block data: \t "+str(rdr.read(sector * 16 + block)))
+            time.sleep(0.1)
         # Always stop crypto1 when done working
         rdr.stop_crypto()
 

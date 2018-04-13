72836403a5c28a5319e7388a06917c95151cac5d
asanghehe <413820400@qq.com>
2018-04-13 15:38:09 +0800

    Update card-read.py

--- card-read.py
+++ card-read.py
@@ -13,7 +13,7 @@ if not error:
       # Auth for block 10 (block 2 of sector 2) using default shipping key A
       if not rdr.card_auth(rdr.auth_a, 10, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], uid):
         # This will print something like (False, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-        for num in range(0,127):
+        for num in range(0,63):
           print("block data: "+str(rdr.read(num)))
         # Always stop crypto1 when done working
         rdr.stop_crypto()

54c2b3206636eb9c2d79a92d32aa3be1e64ad100
asanghehe <413820400@qq.com>
2018-04-13 14:53:18 +0800

    Create card-write.py

---
+++ card-write.py
@@ -0,0 +1,21 @@
+from pirc522 import RFID
+rdr = RFID()
+
+rdr.wait_for_tag()
+(error, tag_type) = rdr.request()
+if not error:
+  print("Tag detected")
+  (error, uid) = rdr.anticoll()
+  if not error:
+    print("UID: " + str(uid))
+    # Select Tag is required before Auth
+    if not rdr.select_tag(uid):
+      # Auth for block 10 (block 2 of sector 2) using default shipping key A
+      if not rdr.card_auth(rdr.auth_a, 10, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], uid):
+        # This will print something like (False, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        print("Writing block 10: " + str(rdr.write(10, [233, 33, 33, 33, 33, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 255])))
+        # Always stop crypto1 when done working
+        rdr.stop_crypto()
+
+# Calls GPIO cleanup
+rdr.cleanup()
